ce11673561279e7e0fbdfabe189e62d2a04d398a
Choi <choi98871071@gmail.com>
2017-01-11 10:37:34 +0900

    로그인 페이지 경로설정

--- src/main/java/com/assa/controller/MemberController.java
+++ src/main/java/com/assa/controller/MemberController.java
@@ -1,5 +1,13 @@
 package com.assa.controller;
 
+@Controller
+@RequestMapping("/member")
 public class MemberController {
 
+	
+	
+	@ReqeustMapping(value="/signUp",method=RequestMethod.GET)
+	public String singUpGET(){
+		return "/member/LoginPage";
+	}
 }
