91bbf9599d7b11e35fb44ccde1ca1387c7bb728d
cshl0ve <choi98871071@gmail.com>
2017-01-11 11:49:35 +0900

    no message

--- src/main/java/com/assa/controller/MemberController.java
+++ src/main/java/com/assa/controller/MemberController.java
@@ -13,13 +13,8 @@ public class MemberController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
 	@RequestMapping(value="/signUp",method=RequestMethod.GET)
-<<<<<<< HEAD
-	public String signUpGET(){
-		
-=======
 	public String singUpGET(){
 		logger.info("/member/signUp");
->>>>>>> 5a03cc022232fb5a60911b9cbf285d46f7e9a453
 		return "/member/LoginPage";
 	}
 }
